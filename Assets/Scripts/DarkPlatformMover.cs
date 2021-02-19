5fa0b64bcedcf9b95b169401b37aca0d4d4b808a
GAVING0TGAME423 <78512752+GAVING0TGAME423@users.noreply.github.com>
2021-02-18 17:12:24 -0800

    2nd Version of Graybox
    Collectables, Moving floor (but doesnt stop on win), no reset, gameplay UI

---
+++ Assets/Scripts/DarkPlatformMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkPlatformMover : MonoBehaviour
+{
+
+    public GameObject DarkPlatform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime *.5f);
+    }
+}
---
+++ Assets/Scripts/PickupScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PickupScript : MonoBehaviour
+{
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject ExitTextObject;
+    public GameObject Exit;
+    public GameObject WinTextObject;
+    
+   
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        ExitTextObject.SetActive(false);
+        Exit.SetActive(false);
+        WinTextObject.SetActive(false);
+       
+    }
+
+    void Update()
+    {  
+            
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+         if (other.gameObject.CompareTag("Pickup"))
+        {
+            count = count + 1;
+            SetCountText();
+            other.gameObject.SetActive(false);
+        }
+        if (other.gameObject.CompareTag("Exit"))
+        {
+            WinTextObject.SetActive(true);
+           
+        }
+              
+    }
+    void SetCountText()
+    {
+        countText.text = "Orbs: " + count.ToString() + "/3";
+        if (count >= 3)
+        {
+            ExitTextObject.SetActive(true);
+            Exit.SetActive(true);
+            
+        }
+    }
+}
